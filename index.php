f453e706fe6f3f5b225cf70f53521109d521096e
Md. Omar Faruk <faruk.pbn@gmail.com>
2017-07-18 00:27:28 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php
-
+    // output headers for the file is downloaded rather than displayed
     header("Content-type: text/csv");
     header("Content-Disposition: attachment; filename=file.csv");
     header("Pragma: no-cache");
     header("Expires: 0");
+
+
+    // data for csv file
 
     $employees = array(
         array("Faruk", "Development", "90000"),
@@ -11,10 +14,14 @@
         array("Jisan", "Marketting", "65000")
     );
 
+    // for output stream
     $file = fopen('php://output', 'w');
+
+    // for column headings
     fputcsv($file, array('Name', 'Department', 'Salary'));
 
     foreach ($employees as $employee) {
+        //use fputcsv() generate correct csv lines from your array
         fputcsv($file, $employee);
     }
 
